6939db46111721d07898404d9433d86f9dcb9c5b
kolya7772011 <kolya7772011@gmail.com>
2026-05-11 16:19:45 +0500

    rthh

---
+++ dilshat/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import dilshat 
+urlpatterns = [
+    path('',dilshat),
+]
---
+++ dilshat/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def dilshat(request):
+    return HttpResponse ('magnumat bar biraq basqa jerge otin')
+    
